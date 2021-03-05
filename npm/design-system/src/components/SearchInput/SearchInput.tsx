d14cecac12a7bda239fb68679592c79b7d597b7a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-05 17:54:23 +1000

    fix linting

--- npm/design-system/src/components/SearchInput/SearchInput.tsx
+++ npm/design-system/src/components/SearchInput/SearchInput.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import styles from './SearchInput.module.scss'
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
-
 import cs from 'classnames'
+import styles from './SearchInput.module.scss'
 
 interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   prefixIcon?: FontAwesomeIconProps['icon']
@@ -12,31 +11,36 @@ interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   inputRef: React.RefObject<HTMLInputElement>
 }
 
-export const SearchInput: React.FC<SearchInputProps> = (props) => (<span className={styles.inputButton}>
-  { props.prefixIcon &&
-      <FontAwesomeIcon
-        className={styles.prefix}
-        icon={props.prefixIcon} /> }
-  <input
-    type="text"
-    ref={props.inputRef}
-    className={cs([styles.searchInput, props.prefixIcon ? styles.hasPrefix : ''])}
-    placeholder={props.placeholder}
-    value={props.value}
-    onChange={props.onChange}
-  />
-  { props.value &&
-      <FontAwesomeIcon
-        data-testid="close"
-        className={styles.suffix}
-        tabIndex={0}
-        icon="times"
-        onClick={props.onSuffixClicked}
-        onKeyPress={(e) => {
-          if (e.key === 'Enter' && props.onSuffixClicked) {
-            props.onSuffixClicked()
-          }
-        }}
-      /> }
-</span>
-)
+export const SearchInput: React.FC<SearchInputProps> = (props) => {
+  const prefixIcon = props.prefixIcon && (
+    <FontAwesomeIcon
+      className={styles.prefix}
+      icon={props.prefixIcon}
+    />
+  )
+
+  return (
+    <span className={styles.inputButton}>
+      {prefixIcon}
+      <input
+        type="text"
+        ref={props.inputRef}
+        className={cs([styles.searchInput, props.prefixIcon ? styles.hasPrefix : ''])}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      {
+        props.value &&
+        <FontAwesomeIcon
+          data-testid="close"
+          className={styles.suffix}
+          tabIndex={0}
+          icon="times"
+          onClick={props.onSuffixClicked}
+          onKeyPress={(e) => e.key === 'Enter' && props.onSuffixClicked?.()}
+        />
+      }
+    </span>
+  )
+}
